70cbd957001f5f82b28b6a0350389d3aadf43978
David <rmbrodett@gmail.com>
2024-01-22 00:18:57 +0800

    Routed Home, and initializes some structures and added some comments

--- LRS_Web_Application/src/App.jsx
+++ LRS_Web_Application/src/App.jsx
@@ -1,5 +1,13 @@
+import './App.css'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import {Home} from './Pages/Home.jsx'
 
 function App() {
+    <Router>
+        <Routes>
+            <Route path='/' Component={<Home/>}/>
+        </Routes>
+    </Router>
 }
 
 export default App
---
+++ LRS_Web_Application/src/Pages/Home.jsx
@@ -0,0 +1,19 @@
+import {Header} from '../components/Header.jsx'
+import {Footer} from '../components/Footer.jsx'
+
+
+function Home(){
+    return(
+        <div className="Home_Container">
+            <Header/> {/* Header and footer is still empty, complete it first in components */}
+                <div className="Home_Body">
+                    {/*Put the contents here*/}
+
+                </div>
+            <Footer/>
+        </div>
+    )
+
+}
+
+export default Home

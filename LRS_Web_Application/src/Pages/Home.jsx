db2384313ffd550616b255569cbdaddb91823f57
David <rmbrodett@gmail.com>
2024-02-11 20:08:16 +0800

    locations Pages

--- LRS_Web_Application/src/Pages/Home.jsx
+++ LRS_Web_Application/src/Pages/Home.jsx
@@ -5,14 +5,14 @@ export function Home(){
     return(
         <>
             <div className="Home_Container">
-                <Header /> {/* Header and footer are still empty, complete it first in components */}
+                <Header/> {/* Header and footer are still empty, complete it first in components */}
                     <div className="Home_Body">
                         {/*Put the contents here*/}
                         {/* We put real images of dlsu's computer lab */}
                         {/*then add texts about computer lab and features(capability) */}
                         
                     </div>
-                <Footer />
+                <Footer/>
             </div>
         </>
     )
--- LRS_Web_Application/src/Pages/location.jsx
+++ LRS_Web_Application/src/Pages/location.jsx
@@ -6,18 +6,21 @@ export function Location(){
 
     return(
         <>
-        <Header/>
-            <div id="locationContent">
-                <div id="campusmap">
-                    <span id="campusmap"></span>
-                </div>
-                <div id="labsmap">
-                    <div id="gokslabs"></div>
-                    <div id="stlslabs"></div>
-                    <div id="brandrewlabs"></div>
-                </div>
+            <div className="LocationContainer">
+                <Header/>
+                    <div id="locationContent">
+                        <div id="campusmap">
+                            <h1>Manila - Campus Map</h1>
+                            <span id="campusmap"></span>
+                        </div>
+                        <div id="labsmap">
+                            <div id="gokslabs"></div>
+                            <div id="stlslabs"></div>
+                            <div id="brandrewlabs"></div>
+                        </div>
+                    </div>
+                <Footer/>
             </div>
-        <Footer/>
         </>
     )
 }

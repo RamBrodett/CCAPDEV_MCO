c8d442488bb1dc8e03873a76d649f382045c71e2
David <rmbrodett@gmail.com>
2024-02-11 23:17:57 +0800

    updated locations

--- LRS_Web_Application/src/Pages/location.jsx
+++ LRS_Web_Application/src/Pages/location.jsx
@@ -1,9 +1,9 @@
+
 import { Header } from "../components/Header"
 import { Footer } from "../components/Footer"
 //import { Link } from 'react-router-dom'
 
 export function Location(){
-
     return(
         <>
             <div className="LocationContainer">
@@ -11,9 +11,10 @@ export function Location(){
                     <div id="locationContent">
                         <div id="campusmap">
                             <h1>Manila - Campus Map</h1>
-                            <span id="campusmap"></span>
+                            <span id="campusmapimg"></span>
                         </div>
                         <div id="labsmap">
+                            <h1>Lab Locations</h1>
                             <div id="gokslabs"></div>
                             <div id="stlslabs"></div>
                             <div id="brandrewlabs"></div>

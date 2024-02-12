6fd8604e1bb4af550a64669d76681d49eb80a43e
David <rmbrodett@gmail.com>
2024-02-12 18:45:52 +0800

    almost done locations

--- LRS_Web_Application/src/Pages/location.jsx
+++ LRS_Web_Application/src/Pages/location.jsx
@@ -15,9 +15,27 @@ export function Location(){
                         </div>
                         <div id="labsmap">
                             <h1>Lab Locations</h1>
-                            <div id="gokslabs"></div>
-                            <div id="stlslabs"></div>
-                            <div id="brandrewlabs"></div>
+                            <div id="gokslabs">
+                                <h4>Gokongwei Computer Laboratory</h4>
+                                <div id="goksdetails">
+                                    <span></span>
+
+                                </div>
+                            </div>
+                            <div id="stlslabs">
+                                <h4>St. La Salle Hall Computer Laboratory</h4>
+                                <div id="stlsdetails">
+                                    <span></span>
+
+                                </div>
+                            </div>
+                            <div id="brandrewlabs">
+                                <h4>Br. Andrew Gonzales Hall Computer Laboratory</h4>
+                                <div id="brandrewdetails">
+                                    <span></span>
+
+                                </div>
+                            </div>
                         </div>
                     </div>
                 <Footer/>

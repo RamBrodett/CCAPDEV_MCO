3a89202797fe0800458256de2b74034585725f38
David <rmbrodett@gmail.com>
2024-02-14 15:53:56 +0800

    added guide and book page, initially implemented layout for Home, full content of home will be published later on.

--- LRS_Web_Application/src/App.jsx
+++ LRS_Web_Application/src/App.jsx
@@ -4,6 +4,8 @@ import { Home } from './Pages/home.jsx'
 import { Login } from './Pages/login.jsx'
 import { Register } from './Pages/register.jsx'
 import { Location } from './Pages/location.jsx'
+import { Guide } from './Pages/guide.jsx'
+import {Book} from './Pages/book.jsx'
 
 function App() {
     return(
@@ -13,6 +15,8 @@ function App() {
                 <Route path='/login' element={<Login/>}/>
                 <Route path='/register' element={<Register/>}/>
                 <Route path='/locations' element={<Location/>}/>
+                <Route path='/guide' element={<Guide/>}/>
+                <Route path='/book' element={<Book />}/>
             </Routes>
         </Router>
     )

2ddac034b589fac21ec6620fa60c49a769699dc3
David <rmbrodett@gmail.com>
2024-02-14 12:44:53 +0800

    locations layout fully implemented, only need details of other labs.
    added page mark for style making it easy to locate
    specific styling for each page
    
    refined user icon size and look

--- LRS_Web_Application/src/Pages/location.jsx
+++ LRS_Web_Application/src/Pages/location.jsx
@@ -19,6 +19,23 @@ export function Location(){
                                 <h4>Gokongwei Computer Laboratory</h4>
                                 <div id="goksdetails">
                                     <span></span>
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th className="RoomNo">Room No.</th>
+                                                <th className="FloorLoc">Floor Location</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td className="RoomNo">306A</td>
+                                                <td className="FloorLoc" rowSpan={2}>Third Floor Gokongwei Hall</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="RoomNo" >306B</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
 
                                 </div>
                             </div>
@@ -26,13 +43,48 @@ export function Location(){
                                 <h4>St. La Salle Hall Computer Laboratory</h4>
                                 <div id="stlsdetails">
                                     <span></span>
-
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th className="RoomNo">Room No.</th>
+                                                <th className="FloorLoc">Floor Location</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td className="RoomNo"></td>
+                                                <td className="FloorLoc"></td>
+                                            </tr>
+                                            <tr>
+                                                <td className="RoomNo"></td>
+                                                <td className="FloorLoc"></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                             <div id="brandrewlabs">
                                 <h4>Br. Andrew Gonzales Hall Computer Laboratory</h4>
                                 <div id="brandrewdetails">
                                     <span></span>
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th className="RoomNo">Room No.</th>
+                                                <th className="FloorLoc">Floor Location</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td className="RoomNo"></td>
+                                                <td className="FloorLoc"></td>
+                                            </tr>
+                                            <tr>
+                                                <td className="RoomNo"></td>
+                                                <td className="FloorLoc"></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
 
                                 </div>
                             </div>

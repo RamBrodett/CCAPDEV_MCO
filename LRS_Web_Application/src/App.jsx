c97a2d6e357e50041d7c833265d6df752a15028e
AisuChantal <chantal_sia@dlsu.edu.ph>
2024-02-15 23:49:04 +0800

    its finally working cant wait to not sleep

--- LRS_Web_Application/src/App.jsx
+++ LRS_Web_Application/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {HashRouter as Router, Routes, Route} from 'react-router-dom'
-import { Home } from './Pages/home.jsx'
+import { Home } from './Pages/Home.jsx'
 import { Login } from './Pages/login.jsx'
 import { Register } from './Pages/register.jsx'
 import { Location } from './Pages/location.jsx'

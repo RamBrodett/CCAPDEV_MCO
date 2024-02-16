ec8f8674a1afa73225ba1d52413628d9b43a7d5f
David <rmbrodett@gmail.com>
2024-02-16 21:42:25 +0800

    j

--- LRS_Web_Application/src/Pages/profile.jsx
+++ LRS_Web_Application/src/Pages/profile.jsx
@@ -1,23 +1,33 @@
-import '../components/style.css'
-import { Header } from "../components/Header"
-import { Footer } from "../components/Footer"
-import { useParams } from 'react-router-dom' 
-import { useState } from 'react';
+import './profile.css';
+import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
-
-
-export function Profile(){
+export function Profile() {
     const { userId } = useParams();
     const [userData, setUserData] = useState(null);
 
-    return(
+    useEffect(() => {
+        fetch(`http://localhost:5173/profile/${userId}`)
+            .then(response => response.json())
+            .then(data => setUserData(data))
+            .catch(error => console.error('Error fetching user data:', error));
+    }, [userId]);
+
+    return (
         <div id="profile_Container">
-            <Header/>
+            <Header />
             <div id="profile_body">
-                
+                {userData ? (
+                    <div className='userprofiletop'>
+                        <h2>{`${userData.Fname} ${userData.Lname}'s Profile`}</h2>
+                        <hr />
+                        {/* Display other user-related information */}
+                    </div>
+                ) : null}
             </div>
             <Footer />
         </div>
-    )
-
+    );
 }

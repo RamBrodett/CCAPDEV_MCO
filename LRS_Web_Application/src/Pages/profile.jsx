6128bfdea28350d5d1c5a512a42209c60aebb536
David <rmbrodett@gmail.com>
2024-02-16 20:55:24 +0800

    d

--- LRS_Web_Application/src/Pages/profile.jsx
+++ LRS_Web_Application/src/Pages/profile.jsx
@@ -2,10 +2,14 @@ import '../components/style.css'
 import { Header } from "../components/Header"
 import { Footer } from "../components/Footer"
 import { useParams } from 'react-router-dom' 
+import { useState } from 'react';
 
 
 
 export function Profile(){
+    const { userId } = useParams();
+    const [userData, setUserData] = useState(null);
+
     return(
         <div id="profile_Container">
             <Header/>

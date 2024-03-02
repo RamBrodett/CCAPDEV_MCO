660fa7e3087c61f0923ce6c7a188e575a4b15a51
David <rmbrodett@gmail.com>
2024-03-02 23:08:17 +0800

    database

--- LRS_API/api.js
+++ LRS_API/api.js
@@ -1,7 +1,7 @@
 require('dotenv').config
-
 const port = process.env.PORT || 3000;
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 

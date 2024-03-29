c4176c261cafc32e6a762dbfa1821d84f56be928
David <rmbrodett@gmail.com>
2024-03-30 04:50:29 +0800

    fixed password update, also added some form validation for the password change'

--- LRS_API/controllers/authController.js
+++ LRS_API/controllers/authController.js
@@ -96,4 +96,26 @@ const handleUserLogout = (req, res) =>{
     res.status(200).json({success: 'Logout successful'})
 }
 
-module.exports = {handleUserLogin, handleUserLogout};
+const hanldeUserPasswordCheck = async (req, res) =>{
+    const {email, password} = req.body;
+
+    try{
+        const user = await User.findOne({email});
+        
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid){
+            return res.status(401).json({message: 'Incorrect password'})
+        }
+        res.status(200).json({ message: 'Password is correct' });
+
+    }catch (error){
+
+        res.status(500).json({message : error.message});
+
+    }
+
+
+
+}
+
+module.exports = {handleUserLogin, handleUserLogout, hanldeUserPasswordCheck};
